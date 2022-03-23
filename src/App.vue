599e50c0291708926e1ef48c4a95cb274fd1ac66
Alexey <ab-vis@mail.ru>
2022-03-23 21:08:18 +0700

    git init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>тест</div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,148 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+const DefinePlugin = require('webpack').DefinePlugin;
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+  dist: path.join(__dirname, 'dist')
+};
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
+
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetWebpackPlugin(),
+      new TerserWebpackPlugin()
+    ];
+  }
+
+  return config;
+};
+
+const filename = (ext) => (isDev ? `${PATHS.assets}/${ext}/[name].${ext}` : `${PATHS.assets}/${ext}/[name].[hash].${ext}`);
+
+module.exports = {
+  context: PATHS.src,
+  mode: 'development',
+  entry: {
+    main: ['@babel/polyfill', './main.js'],
+  },
+  output: {
+    filename: filename('js'),
+    path: PATHS.dist,
+    publicPath: isDev ? '/' : './',
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.scss'],
+    alias: {
+      '@': PATHS.src,
+    },
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 4242,
+    open: true,
+  },
+  devtool: isDev ? 'source-map' : false,
+  plugins: [
+    new ESLintPlugin(),
+    new DefinePlugin({
+      __VUE_OPTIONS_API__: JSON.stringify(true),
+      __VUE_PROD_DEVTOOLS__: JSON.stringify(true),
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, `${PATHS.src}/public/favicon.ico`),
+          to: `${PATHS.dist}`,
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css'),
+    }),
+    new HtmlWebpackPlugin({
+      template: `${PATHS.src}/public/index.html`,
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new VueLoaderPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../../',
+            },
+          },
+          'css-loader',
+          'resolve-url-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          outputPath: `${PATHS.assets}/img/`,
+        },
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        loader: 'file-loader',
+        options: {
+          outputPath: `${PATHS.assets}/fonts/`,
+        },
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loader: {
+            scss: 'vue-style-loader!css-loader!sass-loader',
+          },
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
